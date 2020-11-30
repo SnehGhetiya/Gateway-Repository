d8de3d585bf3c30470a8a2baef9b37c0b5a75afb
Sneh M. Ghetiya <snehpatel690@gmail.com>
2020-11-30 17:38:09 +0530

    Initial Commit

---
+++ 2. Source_Control/2. Source_Control/SourceControlAssignment1/SourceControlAssignment1/CustomAgeValidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SourceControlAssignment1
+{
+    public class CustomAgeValidation : ValidationAttribute
+    {
+        int _minAge = 18;
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                int minimum = (int)value;
+                if (minimum >= _minAge)
+                {
+                    return ValidationResult.Success;
+                }
+            }
+            return new ValidationResult("Age must be greater than 18");
+        }
+    }
+}
---
+++ 2. Source_Control/2. Source_Control/SourceControlAssignment1/SourceControlAssignment1/CustomJoiningDateValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SourceControlAssignment1
+{
+    public class CustomJoiningDateValidation : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime dateTime = Convert.ToDateTime(value);
+            if(dateTime <= DateTime.Now)
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult("Joining Date must be less or equal to the current date");
+        }
+    }
+}
